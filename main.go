a36a0d8b04e51e0e2e578d7ca92b8ad66898e894
daigoron-sword <dsr.daichi@gamil.com>
2022-12-04 23:02:04 +0900

    test

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+// func main() {
+// 	num01 := 123
+// 	var num02 int = 1234567890
+// 	num03 := 1.23
+// 	var num04 float64 = 1.23456789
+// 	fmt.Println(reflect.TypeOf(num01))
+// 	fmt.Println(reflect.TypeOf(num02))
+// 	fmt.Println(reflect.TypeOf(num03))
+// 	fmt.Println(reflect.TypeOf(num04))
+// }
 
 func main() {
-	fmt.Printf("Hello world\n")
+	a := 10
+	b := 1
+	var num_bool bool = a < b
+	fmt.Println(num_bool)
+	fmt.Println(reflect.TypeOf(num_bool))
 }
